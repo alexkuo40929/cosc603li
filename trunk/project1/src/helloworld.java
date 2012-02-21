151dc4e7a256db18de7c5818ee84103e8a5bdaf7
rayli2011@gmail.com <rayli2011@gmail.com@aaa1e446-35be-bcc3-1f6e-35eef009b6a0>
2012-02-21 20:10:32 +0000

    hello world test

---
+++ trunk/project1/src/helloworld.java
@@ -0,0 +1,21 @@
+
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class helloworld.
+ */
+public class helloworld
+{
+    
+    /**
+     * The main method.
+     * hello word class
+     * use system print out to printing the hello world!
+     *
+     * @param args the arguments
+     */
+    public static void main(String[] args)
+    {
+          System.out.println("Hello World!");
+    }
+}
