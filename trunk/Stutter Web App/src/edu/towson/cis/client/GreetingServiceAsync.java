26ba60854d41c9cf12ed431b2f32392459a9f70e
rayli2011@gmail.com <rayli2011@gmail.com@aaa1e446-35be-bcc3-1f6e-35eef009b6a0>
2012-03-07 23:36:33 +0000

    the functional project 80% all the function works.

---
+++ trunk/Stutter Web App/src/edu/towson/cis/client/GreetingServiceAsync.java
@@ -0,0 +1,20 @@
+package edu.towson.cis.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	
+	/**
+	 * Greet server.
+	 *
+	 * @param input the input
+	 * @param callback the callback
+	 * @throws IllegalArgumentException the illegal argument exception
+	 */
+	void greetServer(String input, AsyncCallback<String> callback)
+			throws IllegalArgumentException;
+}
