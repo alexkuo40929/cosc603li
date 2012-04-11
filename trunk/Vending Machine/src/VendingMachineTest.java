a018fa202771cb0a7d37383905a5b782f30ef489
rayli2011@gmail.com <rayli2011@gmail.com@aaa1e446-35be-bcc3-1f6e-35eef009b6a0>
2012-04-11 00:21:31 +0000

    new project for vending machine and create all the junit test case

---
+++ trunk/Vending Machine/src/VendingMachineTest.java
@@ -0,0 +1,52 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class VendingMachineTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAddItem() {
+	VendingMachine machine=new VendingMachine();
+	VendingMachineItem item=new VendingMachineItem("MM", 1.25);
+	
+	
+		
+	}
+
+	@Test
+	public void testRemoveItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertMoney() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBalance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMakePurchase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReturnChange() {
+		fail("Not yet implemented");
+	}
+
+}
