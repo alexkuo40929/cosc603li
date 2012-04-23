c6dd9ab6031577e73caa42fa1edcceeffdaa647f
rayli2011 <rayli2011@gmail.com>
2012-04-23 22:27:00 +0000

    test repoet to make vendingmachine.java and vendingmachineitem.java as 100% coverage

--- Vending Machine/src/VendingMachineTest.java
+++ Vending Machine/src/VendingMachineTest.java
@@ -10,6 +10,7 @@ public class VendingMachineTest {
 	public VendingMachine machine2;
 	public VendingMachineItem item ;
 	public VendingMachineItem item2 ;
+	public VendingMachineItem item3 ;
 	double amount=5.00;
 	
 	@Before
@@ -17,6 +18,7 @@ public class VendingMachineTest {
 		machine=new VendingMachine();
 		item=new VendingMachineItem("MM", 1.25);
 		item2=new VendingMachineItem("car",5.00);
+		item3=new VendingMachineItem("ices",2.00);
 	}
 
 	@After
@@ -30,22 +32,33 @@ public class VendingMachineTest {
 
 	    machine.addItem(item,VendingMachine.A_CODE);
 	    machine.addItem(item2,VendingMachine.B_CODE);
+	    
+	    machine.addItem(item3,VendingMachine.D_CODE);
 	    assertSame(item, machine.getItem(VendingMachine.A_CODE ));
 	    assertSame(item2, machine.getItem(VendingMachine.B_CODE ));
 	 // test case 2 for the slot C and D to see is that still nothing without add to wrong solt
 	    assertNull(machine.getItem(VendingMachine.C_CODE));
-		assertNull(machine.getItem(VendingMachine.D_CODE));
+		assertNotNull(machine.getItem(VendingMachine.D_CODE));
 	}
 	
 	//test the throw VendingMachineException to add different item into same slot
 	@Test(expected=VendingMachineException.class)
 	public void testadditem2() {
+		//test the exception of add some other item to the same slot
 		machine.addItem(item, VendingMachine.A_CODE);
 		machine.addItem(item2, VendingMachine.A_CODE);
+		//machine.addItem(item, VendingMachine.E_CODE);
+	
 	}
 		
-		
-   
+    @Test (expected = VendingMachineException.class)
+    //test invalidate code 
+    public void testAddItem3(){
+            machine.addItem(item, "slotA");   
+            assertNull(machine.getItem("slotA"));
+    }	
+			
+
 	@Test
 	// test the remote function. after tie down, we need add a new item into 
 	//slot first and call remote function to check the value in this slot should be null
@@ -54,7 +67,11 @@ public class VendingMachineTest {
 		machine.removeItem(VendingMachine.A_CODE);
 		assertEquals(null,machine.getItem(VendingMachine.A_CODE ) );
 	}
-
+	@Test(expected=VendingMachineException.class)
+	public void testRemoveItem2(){
+		machine.removeItem(VendingMachine.D_CODE);
+	
+	}
 	
 	
 	@Test
@@ -75,7 +92,11 @@ public class VendingMachineTest {
         assertEquals(4.75, machine.getBalance(), 0.01); 
 	}
 
-	
+	@Test(expected=VendingMachineException.class)
+	public void testInsertMoney2() {
+     machine.insertMoney(-1.0) ;
+     
+	}
 	@Test
 	//test the price of the new item is the same as the add to a new item to the machine
 	public void testGetBalance() {
@@ -95,18 +116,22 @@ public class VendingMachineTest {
 	public void testMakePurchase() {
 		machine.addItem(item, VendingMachine.A_CODE);
 		machine.addItem(item2, VendingMachine.B_CODE);
-		machine.balance=5.0;
+		machine.balance=3.0;
 		assertEquals(true,machine.makePurchase(VendingMachine.A_CODE));
 		//test case of if purchase on the empty slot
 		assertEquals(false,machine.makePurchase(VendingMachine.C_CODE));
 		//after purchase, then test how much left 5.0-1.25, to see is the value changed.
-		assertEquals(3.75,machine.getBalance(),0.01);
+		assertEquals(1.75,machine.getBalance(),0.01);
 		
 		/////////////////////////////////////////////////
-		
+		//test purchase in empty false;
+		machine.balance=3.0;
+		assertEquals(false,machine.makePurchase(VendingMachine.D_CODE));
+        //test purchase with enough money
+		machine.addItem(item2, VendingMachine.C_CODE);
+		machine.balance=3.0;
+		assertEquals(false,machine.makePurchase(VendingMachine.C_CODE));
 	}
-
-
 
 	@Test
 	
